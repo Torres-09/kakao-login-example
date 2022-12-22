bde9e24ed57a3075ec9ea0613ff6fde6c6fc7129
imsehwan <dla612@naver.com>
2022-12-23 03:21:09 +0900

    Initial commit

---
+++ src/main/java/com/example/kakaologinexample/KakaoLoginExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaologinexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoLoginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoLoginExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kakaologinexample/KakaoLoginExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaologinexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoLoginExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
