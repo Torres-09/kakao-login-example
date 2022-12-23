aa2e982dc0cfae2e2bda05db84275700f53a7823
Torres-09 <dla612@naver.com>
2022-12-24 01:25:33 +0900

    feat : User Entity 추가
    회원 엔티티에 소셜 로그인 이후, 회원 이메일로 가입시키기 위함, 빌더패턴 적용

--- src/main/java/com/example/kakaologinexample/domain/User.java
+++ src/main/java/com/example/kakaologinexample/domain/User.java
@@ -1,9 +1,13 @@
 package com.example.kakaologinexample.domain;
 
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "user_table")
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -11,4 +15,10 @@ public class User {
     Long id;
 
     String email;
+
+    @Builder
+    public User(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
 }
