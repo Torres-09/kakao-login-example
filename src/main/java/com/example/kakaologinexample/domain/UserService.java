e626b0c84ec0992ae8b7ab3889134acd77bff23b
Torres-09 <dla612@naver.com>
2022-12-24 01:27:49 +0900

    feat : UserService createUser 로직 추가
    새로운 회원 저장

--- src/main/java/com/example/kakaologinexample/domain/UserService.java
+++ src/main/java/com/example/kakaologinexample/domain/UserService.java
@@ -1,8 +1,23 @@
 package com.example.kakaologinexample.domain;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long createUser(String email) {
+        User user = User.builder()
+                .email(email)
+                .build();
+
+        userRepository.save(user);
+        System.out.println("유저 저장 완료");
+        return user.id;
+    }
 }
