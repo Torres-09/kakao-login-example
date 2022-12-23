4078d6af9b140b2fbf4fddb0a72ca98311be66e1
Torres-09 <dla612@naver.com>
2022-12-23 21:46:26 +0900

    test : ControllerTest 생성
    mockMvc로 API 테스트 예정

---
+++ src/test/java/com/example/kakaologinexample/controller/ControllerTest.java
@@ -0,0 +1,11 @@
+package com.example.kakaologinexample.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class ControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+}
