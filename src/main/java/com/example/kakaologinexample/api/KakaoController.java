f5122e3c7b49b938fa84627d9d6f2927da3e9a6a
Torres-09 <dla612@naver.com>
2022-12-24 01:28:12 +0900

    feat : 이메일로 회원 저장하는 api 최종 추가

--- src/main/java/com/example/kakaologinexample/api/KakaoController.java
+++ src/main/java/com/example/kakaologinexample/api/KakaoController.java
@@ -1,16 +1,25 @@
 package com.example.kakaologinexample.api;
 
-import com.example.kakaologinexample.utils.GetKakaoTokenJsonData;
+import com.example.kakaologinexample.domain.User;
+import com.example.kakaologinexample.domain.UserRepository;
+import com.example.kakaologinexample.domain.UserService;
+import com.example.kakaologinexample.utils.KakaoTokenJsonData;
+import com.example.kakaologinexample.utils.KakaoUserInfo;
 import com.example.kakaologinexample.utils.dto.KakaoTokenResponse;
+import com.example.kakaologinexample.utils.dto.KakaoUserInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 public class KakaoController {
-    private final GetKakaoTokenJsonData kakaoTokenJsonData;
+    private final KakaoTokenJsonData kakaoTokenJsonData;
+    private final KakaoUserInfo kakaoUserInfo;
+
+    private final UserService userService;
 
     @GetMapping("/index")
     public String index() {
@@ -18,11 +27,17 @@ public class KakaoController {
     }
 
     @GetMapping("/oauth")
+    @ResponseBody
     public String kakaoOauth(@RequestParam("code") String code) {
         System.out.println("호출");
         KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
         System.out.println(kakaoTokenResponse);
+        KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(kakaoTokenResponse.getAccess_token(), 1);
+        System.out.println(userInfo);
+
+        userService.createUser(userInfo.getKakao_account().getEmail());
+
         // 됐다.~
-        return null;
+        return "okay";
     }
 }
