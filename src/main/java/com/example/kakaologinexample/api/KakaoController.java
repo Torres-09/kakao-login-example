7f998f83346eb70e250f689176d0cc0b4efd0ec8
Torres-09 <dla612@naver.com>
2022-12-25 20:26:09 +0900

    fix : sout에서 log.info로 로그 변경

--- src/main/java/com/example/kakaologinexample/api/KakaoController.java
+++ src/main/java/com/example/kakaologinexample/api/KakaoController.java
@@ -8,6 +8,8 @@ import com.example.kakaologinexample.utils.KakaoUserInfo;
 import com.example.kakaologinexample.utils.dto.KakaoTokenResponse;
 import com.example.kakaologinexample.utils.dto.KakaoUserInfoResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class KakaoController {
     private final KakaoTokenJsonData kakaoTokenJsonData;
     private final KakaoUserInfo kakaoUserInfo;
@@ -26,18 +29,19 @@ public class KakaoController {
         return "loginForm";
     }
 
+    @Description("회원이 소셜 로그인을 마치면 자동으로 실행되는 API입니다. 인가 코드를 이용해 토큰을 받고, 해당 토큰으로 사용자 정보를 조회합니다." +
+            "사용자 정보를 이용하여 서비스에 회원가입합니다.")
     @GetMapping("/oauth")
     @ResponseBody
     public String kakaoOauth(@RequestParam("code") String code) {
-        System.out.println("호출");
+        log.info("인가 코드를 이용하여 토큰을 받습니다.");
         KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
-        System.out.println(kakaoTokenResponse);
+        log.info("토큰에 대한 정보입니다.{}",kakaoTokenResponse);
         KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(kakaoTokenResponse.getAccess_token(), 1);
-        System.out.println(userInfo);
+        log.info("회원 정보 입니다.{}",userInfo);
 
         userService.createUser(userInfo.getKakao_account().getEmail());
 
-        // 됐다.~
         return "okay";
     }
 }
